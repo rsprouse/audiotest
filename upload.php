fcfc1998e3ee94df359b8469387c4e20f48e387e
Ronald L. Sprouse <ronald@berkeley.edu>
2021-02-24 09:33:42 -0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -7,7 +7,11 @@
 
     $upfile_name = basename($_FILES['file']['name']);
     # Ensure filename is of the pattern:
-    # 1-6 digit subject number + _ + 1-3 digit stimulus number + _ + 1-2 digit rep number + .wav
+    # {subject number}_{stimulus number}_{repetition number}.wav
+    # where
+    #     {subject number} is a 1-6 digit number
+    #     {stimulus number} is a 1-3 digit number
+    #     {repetition number} is a 1-2 digit number
     if (! preg_match('/^\d{1,6}_\d{1,3}_\d{1,2}\.wav$/', $upfile_name) ) {
         error_log("ERROR - invalid filename!" . $upfile_name); 
         exit();
